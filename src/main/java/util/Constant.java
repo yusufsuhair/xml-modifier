38122d2a31331570b8912e20065a9f31fcfc8d27
Mohd Suhair, Yusuf <Yusuf.MohdSuhair@sicpa.com>
2020-10-24 23:20:18 +0800

    dah buat utk config reader

--- src/main/java/util/Constant.java
+++
@@ -1,9 +0,0 @@
-package util;
-
-public class Constant {
-  public static final String XML_DIR = "src/main/resources/SimpleArchiveFormat/item_%s/dublin_core.xml";
-  public static final String XML_ELEMENT = "dcvalue";
-  public static final String FILES_DIRECTORY = "src/main/resources/SimpleArchiveFormat";
-  public static final String XML_NAMED_ITEM = "element";
-  public static final String XML_NAMED_ITEM_2 = "qualifier";
-}
